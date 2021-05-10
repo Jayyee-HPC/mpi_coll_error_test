10755386557ebe8769ea1804afc2225f79c9b200
Jie Yang <jie.yang@marquette.edu>
2021-05-10 11:00:15 -0500

    update test script for all collectives

--- auto_test.sh
+++ auto_test.sh
@@ -42,12 +42,22 @@ done
 echo "**************END Gather error tests*******************"
 
 echo -e	
+echo "**************Perform Gatherv error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 3 $i
+done
+echo "**************END Gatherv error tests*******************"
+
+echo -e	
 echo "**************Perform Reduce error tests*******************"
 
 for ((i=0 ; i<8 ; i++)); do
 	echo -e	
 	echo -e "\tTESTCASE $i"
-	test_func $timeout $num_proc 3 $i
+	test_func $timeout $num_proc 4 $i
 done
 echo "**************END Reduce error tests*******************"
 
@@ -57,7 +67,7 @@ echo "**************Perform Scan error tests*******************"
 for ((i=0 ; i<7 ; i++)); do
 	echo -e	
 	echo -e "\tTESTCASE $i"
-	test_func $timeout $num_proc 4 $i
+	test_func $timeout $num_proc 5 $i
 done
 echo "**************END Scan error tests*******************"
 
@@ -67,7 +77,7 @@ echo "**************Perform Scatter error tests*******************"
 for ((i=0 ; i<7 ; i++)); do
 	echo -e	
 	echo -e "\tTESTCASE $i"
-	test_func $timeout $num_proc 5 $i
+	test_func $timeout $num_proc 6 $i
 done
 echo "**************END Scatter error tests*******************"
 
@@ -77,18 +87,209 @@ echo "**************Perform Scatterv error tests*******************"
 for ((i=0 ; i<7 ; i++)); do
 	echo -e	
 	echo -e "\tTESTCASE $i"
-	test_func $timeout $num_proc 6 $i
+	test_func $timeout $num_proc 7 $i
 done
 echo "**************END Scatterv error tests*******************"
 
 echo -e	
-echo "**************Perform Igather error tests*******************"
+echo "**************Perform Allgather error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 8 $i
+done
+echo "**************END Allgather error tests*******************"
+
+echo -e	
+echo "**************Perform Allgatherv error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 9 $i
+done
+echo "**************END Allgatherv error tests*******************"
+
+echo -e	
+echo "**************Perform Allreduce error tests*******************"
 
 for ((i=0 ; i<7 ; i++)); do
 	echo -e	
 	echo -e "\tTESTCASE $i"
-	test_func $timeout $num_proc 7 $i
+	test_func $timeout $num_proc 10 $i
 done
-echo "**************END Igather error tests*******************"
+echo "**************END Allreduce error tests*******************"
+
+echo -e	
+echo "**************Perform Alltoall error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 11 $i
+done
+echo "**************END Alltoall error tests*******************"
+
+echo -e	
+echo "**************Perform Alltoallv error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 12 $i
+done
+echo "**************END Alltoallv error tests*******************"
+
+echo -e	
+echo "**************Perform Alltoallw error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 13 $i
+done
+echo "**************END Alltoallw error tests*******************"
+
+
+echo -e	
+echo "**************Perform Ibcast error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 14 $i
+done
+echo "**************END IBcast error tests*******************"
+
+echo -e	
+echo "**************Perform IExscan error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 15 $i
+done
+echo "**************END IExscan error tests*******************"
+
+echo -e	
+echo "**************Perform IGather error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 16 $i
+done
+echo "**************END IGather error tests*******************"
+
+echo -e	
+echo "**************Perform IGatherv error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 17 $i
+done
+echo "**************END IGatherv error tests*******************"
+
+echo -e	
+echo "**************Perform IReduce error tests*******************"
+
+for ((i=0 ; i<8 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 18 $i
+done
+echo "**************END IReduce error tests*******************"
+
+echo -e	
+echo "**************Perform IScan error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 19 $i
+done
+echo "**************END IScan error tests*******************"
+
+echo -e	
+echo "**************Perform IScatter error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 20 $i
+done
+echo "**************END IScatter error tests*******************"
+
+echo -e	
+echo "**************Perform IScatterv error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 21 $i
+done
+echo "**************END IScatterv error tests*******************"
+
+echo -e	
+echo "**************Perform IAllgather error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 22 $i
+done
+echo "**************END IAllgather error tests*******************"
+
+echo -e	
+echo "**************Perform IAllgatherv error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 23 $i
+done
+echo "**************END IAllgatherv error tests*******************"
+
+echo -e	
+echo "**************Perform IAllreduce error tests*******************"
+
+for ((i=0 ; i<7 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 24 $i
+done
+echo "**************END IAllreduce error tests*******************"
+
+echo -e	
+echo "**************Perform IAlltoall error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 25 $i
+done
+echo "**************END IAlltoall error tests*******************"
+
+echo -e	
+echo "**************Perform IAlltoallv error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 26 $i
+done
+echo "**************END IAlltoallv error tests*******************"
+
+echo -e	
+echo "**************Perform IAlltoallw error tests*******************"
+
+for ((i=0 ; i<6 ; i++)); do
+	echo -e	
+	echo -e "\tTESTCASE $i"
+	test_func $timeout $num_proc 27 $i
+done
+echo "**************END IAlltoallw error tests*******************"
 
 echo "END AUTO TESTS."
